fdcb824971f4459989bda74849f99d312dfa9beb
yashgit <yashkr10316@gmail.com>
2026-01-31 04:26:30 +0530

    configuration of db

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
